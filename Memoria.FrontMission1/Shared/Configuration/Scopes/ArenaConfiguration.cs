5267c56ce15e0e56912090bc624371d42828dd99
Albeoris <albeoris@gmail.com>
2023-07-28 02:02:12 +0300

    Readme fix

--- Memoria.FrontMission1/Shared/Configuration/Scopes/ArenaConfiguration.cs
+++ Memoria.FrontMission1/Shared/Configuration/Scopes/ArenaConfiguration.cs
@@ -5,8 +5,8 @@ namespace Memoria.FrontMission1.Configuration;
 [ConfigScope("Arena")]
 public abstract partial class ArenaConfiguration
 {
-    [ConfigEntry("If enabled, each pilot will be able to fight only one time with each opponent in the arena." +
-                 "$[Russian]: При включении, каждый пилот сможет сразиться лишь один раз с каждым противником на арене.")]
+    [ConfigEntry("If enabled, each pilot will be able to fight only one time with each opponent in the arena. You no longer lose money when you lose." +
+                 "$[Russian]: При включении, каждый пилот сможет сразиться лишь один раз с каждым противником на арене. Вы больше не теряете деньги при поражении.")]
     public virtual Boolean DisableRepeatedBattles => false;
 
     public abstract void CopyFrom(ArenaConfiguration configuration);
